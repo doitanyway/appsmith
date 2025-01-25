e0fc33dfe6f537c55e6fa97ea136cc1600a32758
Nick <qiujiahongde@163.com>
2025-01-25 15:27:28 +0800

    实现用户页面静态爷儿们

--- app/client/src/ce/pages/UserList/UserListPage.tsx
+++ app/client/src/ce/pages/UserList/UserListPage.tsx
@@ -1,28 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import {Callout, Divider, Icon, Text, Tooltip} from "@appsmith/ads";
-import {
-  ADMIN_AUTH_SETTINGS_SUBTITLE,
-  ADMIN_AUTH_SETTINGS_TITLE,
-  AUTHENTICATION_METHOD_ENABLED,
-  createMessage
-} from "../../constants/messages";
-import EnterpriseTag from "../../../components/EnterpriseTag";
-import {ActionButton} from "../../../pages/AdminSettings/Authentication/AuthPage";
-
-// export const Container = styled.div`
-//   background-color: var(--ads-v2-color-bg);
-//   padding: var(--ads-v2-spaces-7);
-//   min-height: 0;
-//   width: 100%;
-//   position: relative;
-//
-//   .scroll-container {
-//     height: calc(100vh - 50px - 112px);
-//     overflow: auto;
-//   }
-// `;
-
+import {Table, Button, Input, Text} from "@appsmith/ads";
+import {createMessage, UPGRADE} from "../../constants/messages";
 export const Wrapper = styled.div`
   flex-basis: calc(100% - ${(props) => props.theme.homePage.leftPane.width}px);
   padding: var(--ads-v2-spaces-7);
@@ -33,17 +12,63 @@ export const Wrapper = styled.div`
 export const SettingsFormWrapper = styled.div`
   max-width: 40rem;
 `;
-
 export const SettingsHeader = styled(Text)``;
-
 export const SettingsSubHeader = styled(Text)`
   margin-bottom: 24px;
 `;
 
 
+export const OperationWrapper = styled.div`
+  max-width: 40rem;
+  display: flex;
+  gap: 16px; /* 设置子元素之间的间距，例如 16px */
+  margin-bottom: 24px;
+`;
+
+const columns = [
+  {
+    title: "用户名",
+    dataIndex: "name",
+  },
+  {
+    title: "邮箱",
+    dataIndex: "email",
+  },
+  {
+    title: "操作",
+    dataIndex: "address",
+    render: (value: { city: string }) => `${value.city}`,
+    sortBy: "city",
+  },
+];
+
+const fakeData = [
+  {
+    key: "1",
+    name: "Ash",
+    email: "Ash@163.com",
+    address: { city: "New York" },
+  },
+  {
+    key: "2",
+    name: "Jane",
+    email: "Jane@163.com",
+    address: { city: "Los Angeles" },
+  },
+  {
+    key: "3",
+    name: "Doe",
+    email: "Doe@163.com",
+    address: { city: "Chicago" },
+  },
+];
 
 export default function UserListPage() {
   // const { carousel, footer, header } = props;
+  const onClickNewUser =()=>{
+    console.log("onClickNewUser")
+  }
+
 
   return (
     <Wrapper>
@@ -62,6 +87,16 @@ export default function UserListPage() {
         >
           用户配置
         </SettingsSubHeader>
+        <OperationWrapper>
+          <Input  size="md"  className="space-y-4" placeholder={"输入搜索"} ></Input>
+          <Button data-testid="t--button-upgrade" onClick={onClickNewUser} size="md">
+            搜索
+          </Button>
+          <Button data-testid="t--button-upgrade" onClick={onClickNewUser} size="md">
+            新增用户
+          </Button>
+        </OperationWrapper>
+        <Table columns={columns} data={fakeData} isSortable />
       </SettingsFormWrapper>
     </Wrapper>
   );
